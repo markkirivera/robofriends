613734b4a01110e50e398cd0bc6c79c2d1766c65
markkirivera <markbalazo14@gmail.com>
2019-01-31 18:20:52 +0800

    Updates

---
+++ precache-manifest.afad05b2684e26d1a4f506041bc42956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "4deb0269a0d2680995fc",
+    "url": "/robofriends/static/js/main.4deb0269.chunk.js"
+  },
+  {
+    "revision": "9002a721b365554dca61",
+    "url": "/robofriends/static/js/1.9002a721.chunk.js"
+  },
+  {
+    "revision": "4deb0269a0d2680995fc",
+    "url": "/robofriends/static/css/main.fa57dca1.chunk.css"
+  },
+  {
+    "revision": "9002a721b365554dca61",
+    "url": "/robofriends/static/css/1.b3e0dcc3.chunk.css"
+  },
+  {
+    "revision": "411b21739567083c15d05aa3b12d5041",
+    "url": "/robofriends/index.html"
+  }
+];
